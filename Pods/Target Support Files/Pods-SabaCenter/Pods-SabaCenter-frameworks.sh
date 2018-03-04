4eec3440e2c1ec3fb28ed404d18166ac940267eb
Ali Hosseini Khayat <ali.hosseini.khayat@live.com>
2018-03-03 23:49:47 -0800

    chore: add PromiseKit pod for async

--- Pods/Target Support Files/Pods-SabaCenter/Pods-SabaCenter-frameworks.sh
+++ Pods/Target Support Files/Pods-SabaCenter/Pods-SabaCenter-frameworks.sh
@@ -135,12 +135,14 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SuperDelegate/SuperDelegate.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Swinject/Swinject.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SuperDelegate/SuperDelegate.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Swinject/Swinject.framework"
