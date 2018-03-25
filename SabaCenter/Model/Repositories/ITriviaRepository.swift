f8373627b61990e1fa5c38a35ffefb667be28b10
Ali Hosseini Khayat <ali.hosseini.khayat@live.com>
2018-03-24 17:52:00 -0700

    feat(repositories): add initial repository interfaces

---
+++ SabaCenter/Model/Repositories/ITriviaRepository.swift
@@ -0,0 +1,13 @@
+//
+//  ITriviaRepository.swift
+//  SabaCenter
+//
+//  Created by Ali Hosseini Khayat on 3/24/18.
+//  Copyright © 2018 Saba. All rights reserved.
+//
+
+import Foundation
+
+public protocol ITriviaRepository {
+
+}
