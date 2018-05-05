ed362e15e2666df7248bac3915f90164659c2bc3
Ali Hosseini Khayat <ali.hosseini.khayat@live.com>
2018-05-05 15:41:33 -0700

    fix(ui): arrange screen layouts per design

--- SabaCenter/AppDelegate.swift
+++ SabaCenter/AppDelegate.swift
@@ -37,24 +37,6 @@ class AppDelegate: SuperDelegate, ApplicationLaunched {
 
     func loadInterface(launchItem: LaunchItem) {
         // Override point for customization after application launch.
-        // swiftlint:disable force_cast
-        let splitViewController = window!.rootViewController as! UISplitViewController
-        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
-        // swiftlint:enable force_cast
-        navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
-        splitViewController.delegate = self
-    }
-}
-
-extension AppDelegate: UISplitViewControllerDelegate {
-    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
-        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
-        guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
-        if topAsDetailController.detailItem == nil {
-            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
-            return true
-        }
-        return false
     }
 }
 
