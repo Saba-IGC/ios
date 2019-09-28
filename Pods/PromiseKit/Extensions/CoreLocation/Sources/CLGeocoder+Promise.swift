30c9d44967ec1f70db62ceb840ab0bddaeba3f38
Ali Hosseini Khayat <ali-hk@users.noreply.github.com>
2019-09-28 11:19:29 -0700

    build: upgrade cocoa pod dependencies

--- Pods/PromiseKit/Extensions/CoreLocation/Sources/CLGeocoder+Promise.swift
+++ Pods/PromiseKit/Extensions/CoreLocation/Sources/CLGeocoder+Promise.swift
@@ -25,6 +25,7 @@ extension CLGeocoder {
     }
 
     /// Submits a forward-geocoding request using the specified address dictionary.
+    @available(iOS, deprecated: 11.0)
     public func geocode(_ addressDictionary: [String: String]) -> Promise<[CLPlacemark]> {
         return Promise { seal in
             geocodeAddressDictionary(addressDictionary, completionHandler: seal.resolve)
@@ -59,6 +60,14 @@ extension CLGeocoder {
     public func geocodePostalAddress(_ postalAddress: CNPostalAddress, preferredLocale locale: Locale?) -> Promise<[CLPlacemark]> {
         return Promise { seal in
             geocodePostalAddress(postalAddress, preferredLocale: locale, completionHandler: seal.resolve)
+        }
+    }
+
+    /// Submits a reverse-geocoding request for the specified location and a preferred locale.
+    @available(iOS 11.0, OSX 10.13, watchOS 4.0, *)
+    public func reverseGeocode(location: CLLocation, preferredLocale locale: Locale?) -> Promise<[CLPlacemark]> {
+        return Promise { seal in
+            reverseGeocodeLocation(location, preferredLocale: locale, completionHandler: seal.resolve)
         }
     }
 #endif
